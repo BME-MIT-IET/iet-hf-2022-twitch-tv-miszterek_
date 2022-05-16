a018979907e4dccc77f7635045c3ca302323c242
benceboros <benceboros140@gmail.com>
2022-05-16 16:08:24 +0200

    GameBoardTeszt osztaly letrehozasa

---
+++ app/src/test/java/peczedavid/nhf/model/GameBoardTest.kt
@@ -0,0 +1,74 @@
+package peczedavid.nhf.model
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class GameBoardTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun getGameBoardBefore() {
+    }
+
+    @Test
+    fun setGameBoardBefore() {
+    }
+
+    @Test
+    fun getPointsBefore() {
+    }
+
+    @Test
+    fun setPointsBefore() {
+    }
+
+    @Test
+    fun getPoints() {
+    }
+
+    @Test
+    fun setPoints() {
+    }
+
+    @Test
+    fun getGameEnded() {
+    }
+
+    @Test
+    fun setGameEnded() {
+    }
+
+    @Test
+    fun newGame() {
+    }
+
+    @Test
+    fun stepBack() {
+    }
+
+    @Test
+    fun move() {
+    }
+
+    @Test
+    fun loadState() {
+    }
+
+    @Test
+    fun saveState() {
+    }
+
+    @Test
+    fun getValue() {
+    }
+}
